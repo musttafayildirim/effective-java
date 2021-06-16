d702c83ebed65db58ddb388711365e697c4e84c9
mustafa <musttafayildirim@gmail.com>
2021-06-16 19:07:08 +0300

    Final'ın önemi ve üst sınıftaki bir abstract metodun üst sınıfın constructor'ı içerisinde kullanılmaması gerektiği üzerinde duruldu.

---
+++ src/main/java/com/mycompany/effectivejavadersi/bolum2/Ders5_Super.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.effectivejavadersi.bolum2;
+
+/**
+ *
+ * @author mustafa
+ */
+public abstract class Ders5_Super {
+    
+    public Ders5_Super() {
+        overrideMe();
+    }
+    
+    public abstract void overrideMe();
+      
+}
